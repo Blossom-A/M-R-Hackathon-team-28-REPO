260de8b8b4bbfeeb45a7503935957578bfcb20bf
Its-JustSbu <116superstm@gmail.com>
2025-07-29 23:58:34 +0200

    pushing for fun

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route
           key="dashboard"
